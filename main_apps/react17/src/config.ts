7e760b56edf57b38e4ce9ae43f88bb6cb0ec4a9f
张章 <hzzhangzhang1@corp.netease.com>
2022-09-22 17:10:33 +0800

    misc: 代码优化

--- main_apps/react17/src/config.ts
+++ main_apps/react17/src/config.ts
@@ -1,14 +1,12 @@
 
 // 开发环境地址
 const config: Record<string, string> = {
-  angular11: 'http://localhost:4001',
   nextjs11: 'http://localhost:4002',
   nuxtjs2: 'http://localhost:4003',
   react16: 'http://localhost:4004',
   react17: 'http://localhost:4005',
   sidebar: 'http://localhost:4006',
   vite: 'http://localhost:4007',
-  vue2: 'http://localhost:4008',
   vue3: 'http://localhost:4009',
 }
 
